8dac1c54dc516499c4f4205c2880b591341ecd2d
Ved-Prakash-74 <96818066+Ved-Prakash-74@users.noreply.github.com>
2022-04-03 19:40:45 +0530

    Create biggest_of_two_number_using_if_else.py

---
+++ DECISION MAKING CONTROL STATEMENT/biggest_of_two_number_using_if_else.py
@@ -0,0 +1,8 @@
+#python program to print biggest of two numbers using if-else
+
+a=float(input("Enter 1st number:"))
+b=float(input("Enter 2nd number:"))
+if(a>b):
+    print(a,"is greater than",b)
+else:
+    print(b,"is greater than",a)
