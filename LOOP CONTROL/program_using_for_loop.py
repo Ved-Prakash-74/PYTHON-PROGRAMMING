07f683122ef9c20e0a1c98963e7970f5611b07ca
Ved-Prakash-74 <96818066+Ved-Prakash-74@users.noreply.github.com>
2022-04-05 14:42:48 +0530

    Create program_using_for_loop.py

---
+++ LOOP CONTROL/program_using_for_loop.py
@@ -0,0 +1,29 @@
+#for loop for string
+s="python"
+for i in s:
+    print(i)
+
+#for loop for a range
+print("\n")
+for i in range(1,5,2):
+    print(i)
+
+print("\n")
+for i in range(1,5):
+    print(i)
+
+print("\n")
+for i in range(5):
+    print(i)
+
+#for loop for list
+print("\n")
+a=[10,2.5,"abc"]
+for i in a:
+    print(i)
+
+#for loop for tuple
+print("\n")
+a=(10,2.5,"abc")
+for i in a:
+    print(i)
