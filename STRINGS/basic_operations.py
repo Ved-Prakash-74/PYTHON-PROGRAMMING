cf3004a225b4df6a98599a343e92427fc030a1e5
Ved-Prakash-74 <96818066+Ved-Prakash-74@users.noreply.github.com>
2022-04-07 15:43:09 +0530

    Create basic_operations.py

---
+++ STRINGS/basic_operations.py
@@ -0,0 +1,20 @@
+#basic operations on string are:
+
+# i) concatenation
+s1="string 1"
+s2="string 2"
+s3=s1+s2
+print(s3)
+
+# ii) repetition
+print("\n")
+s="string"
+print(s*3)
+
+# iii) slicing
+print("\n")
+s="python"
+print(s[0:4:1])
+print(s[0:5:2])
+print(s[2::])
+print(s[:4:])
